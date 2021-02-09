031c5ae8126df02ba0ec737f7fb161bc407cc147
Umar Khalid <umar.qur105@gmail.com>
2021-02-09 16:15:00 +0500

    components render on screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./App.css";
-import Task from "./components/Task";
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./lib/redux";
 
+import InboxScreen from "./components/InboxScreen";
+
+import "./index.css";
 function App() {
 	return (
-		<div className="App">
-			<Task />
-		</div>
+		<Provider store={store}>
+			<InboxScreen />
+		</Provider>
 	);
 }
-
 export default App;
